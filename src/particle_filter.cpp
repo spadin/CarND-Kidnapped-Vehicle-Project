e4813eb62ad7baa0b2a74caf554904f08335a906
Sandro Padin <sandropadin@gmail.com>
2017-05-19 12:08:15 -0700

    Implement ParticleFilter::resample

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -195,6 +195,22 @@ void ParticleFilter::resample()
   // TODO: Resample particles with replacement with probability proportional to their weight.
   // NOTE: You may find std::discrete_distribution helpful here.
   //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  std::default_random_engine gen;
+
+  std::discrete_distribution<double> distribution(weights.begin(), weights.end());
+
+  std::vector<Particle> resampledParticles;
+
+  for (int i = 0; i < num_particles; i++) {
+    particles[i].weight = weights[i];
+  }
+
+  for(int i = 0; i < num_particles; i++) {
+    int index = distribution(gen);
+    resampledParticles.push_back(particles[index]);
+  }
+
+  particles = resampledParticles;
 }
 
 void ParticleFilter::write(std::string filename)
